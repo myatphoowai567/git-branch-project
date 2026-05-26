900092ad4ffe7a4e69e04eb679e0e1889ec2ca88
myatphoowai567 <myatphoowai567@gmail.com>
2026-05-26 16:30:24 +0630

    add login feature

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => {
     res.send("Home Page");
 });
 
+// NEW FEATURE (LOGIN)
+app.get("/login", (req, res) => {
+    res.send("Login Page");
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
